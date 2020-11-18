97273fc4aa75b73e6aae391e762ae9c92fd1dba9
envomp <enrico.vompa@gmail.com>
2020-11-18 15:38:36 +0200

    refactor ForecastReport implementation

--- src/main/java/ee/taltech/weather/io/report/ForecastReport.java
+++ src/main/java/ee/taltech/weather/io/report/ForecastReport.java
@@ -1,5 +1,6 @@
 package ee.taltech.weather.io.report;
 
+import ee.taltech.weather.api.report.WeatherReportDTO;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -8,5 +9,15 @@ import lombok.NoArgsConstructor;
 @Getter
 @Builder
 @NoArgsConstructor
+@AllArgsConstructor
 public class ForecastReport {
+	private String date;
+	private Weather weather;
+
+	public static ForecastReport from(WeatherReportDTO dto) {
+		return ForecastReport.builder()
+				.date(dto.getDt().toLocalDate().toString())
+				.weather(Weather.from(dto))
+				.build();
+	}
 }
