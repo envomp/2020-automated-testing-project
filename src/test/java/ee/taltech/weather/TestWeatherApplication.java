7da6dd69837326386c6b385fc9c38a3c045f843b
envomp <enrico.vompa@gmail.com>
2020-11-30 21:49:42 +0200

    Custom test entrypoint

---
+++ src/test/java/ee/taltech/weather/TestWeatherApplication.java
@@ -0,0 +1,13 @@
+package ee.taltech.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestWeatherApplication.class, args);
+	}
+
+}
